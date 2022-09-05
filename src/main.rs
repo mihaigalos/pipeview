8a856ed0550028b9426c22ffbe7b187b3f7d18a2
Mihai Galos <mihaigalos@gmail.com>
2022-09-05 19:16:18 +0200

    Args: Raw mode

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@ use pipeview::bar::DEFAULT_PIPEVIEW_SIZE;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _: Command = autoclap!();
+    let app: Command = autoclap!();
+    let _ = app
+        .arg(
+            Arg::new("raw")
+                .long("raw")
+                .short('r')
+                .help("Current, min, max stats from raw data as float."),
+        )
+        .try_get_matches()
+        .unwrap_or_else(|e| e.exit());
+
     let mut pipeview = pipeview::bar::WrappedBar::new(DEFAULT_PIPEVIEW_SIZE);
 
     loop {
