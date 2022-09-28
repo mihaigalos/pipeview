0e3f9ab0f6b8be3ac8a18e8f8e9f8dd7d8c6005f
Mihai Galos <mihaigalos@gmail.com>
2022-09-28 20:29:51 +0200

    Colorizer: colorize() returns Vec

--- src/colorizer.rs
+++ src/colorizer.rs
@@ -7,30 +7,26 @@ pub fn colorize<'a>(
     input: &'a str,
     regex: &'a str,
     colors: &'a str,
-) -> Result<ColoredString, &'static str> {
+) -> Result<Vec<ColoredString>, &'static str> {
     let colors: Vec<&'a str> = colors.split(" ").collect();
 
     let re = Regex::new(regex).unwrap();
     let caps = re.captures(input).ok_or("Cannot apply regex")?;
-    let mut result = "".to_string();
+    let mut result: Vec<ColoredString> = vec![];
 
-    let mut i = 0;
-    for e in caps.iter() {
-        if i == 0 {
-            i = i + 1;
-            continue;
-        }
-        let group = e.ok_or("No element")?.as_str();
-        println!("{}", group);
+    let caps = all_captures_except_first(caps)?;
 
-        let colored_group = match colors[i] {
-            "cyan" => group.cyan(),
+    for (pos, e) in caps.iter().enumerate() {
+        let colored_group = match colors[pos] {
+            "cyan" => e.cyan(),
+            "magenta" => e.magenta(),
             _ => ColoredString::from(""),
         };
-        result += &colored_group;
+        println!("{} -> {}", e, colored_group);
+        result.push(colored_group);
     }
 
-    Ok(result.cyan())
+    Ok(result)
 }
 
 fn all_captures_except_first<'a>(input: regex::Captures) -> Result<Vec<String>, &'static str> {
@@ -56,13 +52,9 @@ mod tests {
     fn test_colorize_works_when_typical() {
         let input = "abc de";
         let expected1 = ColoredString::from("abc").cyan();
-        let expected2 = ColoredString::from("de").cyan();
+        let expected2 = ColoredString::from("de").magenta();
 
-        let result: Vec<ColoredString> = colorize(input, "(.*) (.*)", "cyan magenta")
-            .unwrap()
-            .split(' ')
-            .map(|s| ColoredString::from(s))
-            .collect();
+        let result: Vec<ColoredString> = colorize(input, "(.*) (.*)", "cyan magenta").unwrap();
 
         assert_eq!(result[0], expected1);
         assert_eq!(result[1], expected2);
