c6267b01cda6df563622e03192a59c8b643aa257
Mihai Galos <mihai.galos@rbinternational.com>
2024-08-28 21:30:59 +0200

    feat: IO - Pipes

---
+++ src/constants.rs
@@ -0,0 +1,10 @@
+pub const BUFFER_SIZE: usize = 4096;
+pub const STATS_TIMER_RESOLUTION_MS: u64 = 100;
+
+pub const BYTE: (f64, &str) = (1., "B");
+pub const KILO: (f64, &str) = (1024., "KB");
+pub const MEGA: (f64, &str) = (1024. * KILO.0, "MB");
+pub const GIGA: (f64, &str) = (1024. * MEGA.0, "GB");
+pub const TERRA: (f64, &str) = (1024. * GIGA.0, "TB");
+pub const PETA: (f64, &str) = (1024. * TERRA.0, "PB");
+pub const EXA: (f64, &str) = (1024. * PETA.0, "EB");
